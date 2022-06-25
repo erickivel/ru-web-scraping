6de519ec7b9b87244205d38ed37937ba2da88107
Eric Kivel <ericvkivel1@gmail.com>
2022-06-24 23:56:36 -0300

    Get wek days

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 
 const main = async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto("https://pra.ufpr.br/ru/ru-centro-politecnico/");
 
@@ -18,41 +18,91 @@ const main = async () => {
       "tbody > tr:nth-child(5)"
     );
 
-    const titles = [
-      breakfastTitle[0].innerText,
-      lunchTitle[0].innerText,
-      dinnerTitle[0].innerText,
-    ];
+    const breakfastHTML: any = document.querySelectorAll(
+      "tbody > tr:nth-child(2)"
+    );
+    const lunchHTML: any = document.querySelectorAll("tbody > tr:nth-child(4)");
+    const dinnerHTML: any = document.querySelectorAll(
+      "tbody > tr:nth-child(6)"
+    );
+    const daysHTML: any = document.querySelectorAll("p > strong > strong");
 
-    const breakfast: any = document.querySelectorAll("tbody > tr:nth-child(2)");
-    const lunch: any = document.querySelectorAll("tbody > tr:nth-child(4)");
-    const dinner: any = document.querySelectorAll("tbody > tr:nth-child(6)");
+    //const days = [daysHTML[0].innerText, daysHTML[1].innerText];
+    const days = [daysHTML[0].innerText];
 
-    const menus = [
-      breakfast[0].innerText,
-      lunch[0].innerText,
-      dinner[0].innerText,
-    ];
+    const titles = days.map((_, index) => {
+      return {
+        breakfast: breakfastTitle[index].innerText,
+        lunch: lunchTitle[index].innerText,
+        dinner: dinnerTitle[index].innerText,
+      };
+    });
+
+    const menus = days.map((_, index) => {
+      return {
+        breakfast: breakfastHTML[index].innerText,
+        lunch: lunchHTML[index].innerText,
+        dinner: dinnerHTML[index].innerText,
+      };
+    });
+
+    const splitted = menus[0].lunch
+      .split("\n")
+      .map((meal: string) => meal.trim());
+    console.log(splitted);
+
+    const fig = document.querySelector("p + figure")?.childNodes;
+    console.log(fig);
+    return fig;
+    //let menuResponse = [];
+
+    //if (breakfastHTML.length === 2) {
+    //menuResponse = days.map((_, index) => {
+    //return {
+    //weekDay: days[index],
+    //breakfast: {
+    //title: titles[index].breakfast,
+    //menu: menus[index].breakfast,
+    //},
+    //lunch: {
+    //title: titles[index].lunch,
+    //menu: menus[index].lunch,
+    //},
+    //dinner: {
+    //title: titles[index].dinner,
+    //menu: menus[index].dinner,
+    //},
+    //};
+    //});
+    //} else {
+    //// Checks if the first menu day has a table withe the menu
+    //if (document.querySelector("p + figure")?.children[0] === table);
+    //}
+
+    //return menuResponse;
 
     return {
-      breakfast: {
-        title: titles[0],
-        menu: menus[0],
-      },
-      lunch: {
-        title: titles[1],
-        menu: menus[1],
-      },
-      dinner: {
-        title: titles[2],
-        menu: menus[2],
+      day1: {
+        weekDay: days[0],
+        breakfast: {
+          title: titles[0],
+          menu: menus[0],
+        },
+        lunch: {
+          title: titles[1],
+          menu: menus[1],
+        },
+        dinner: {
+          title: titles[2],
+          menu: menus[2],
+        },
       },
     };
   });
 
   console.log(response);
 
-  await browser.close();
+  //await browser.close();
 };
 
 main();
