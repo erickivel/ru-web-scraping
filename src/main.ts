5e3eaab4de7c8ee08cc94aab9a961bce3b45d2c6
Eric Kivel <ericvkivel1@gmail.com>
2022-06-25 17:19:46 -0300

    Response Parsed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 
 const main = async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://pra.ufpr.br/ru/ru-centro-politecnico/");
 
@@ -25,12 +25,29 @@ const main = async () => {
     const dinnerHTML: any = document.querySelectorAll(
       "tbody > tr:nth-child(6)"
     );
-    const daysHTML: any = document.querySelectorAll("p > strong");
+    let daysHTML: any = document.querySelectorAll("figure + p > strong");
 
-    const availableTables = 
+    const numberOfTables = document.querySelectorAll("table").length;
 
-    //const days = [daysHTML[0].innerText, daysHTML[1].innerText];
-    const days = [daysHTML[1].innerText];
+    let days = [""];
+
+    if (numberOfTables === 1) {
+      // Checks if the first day has a table
+      if (
+        document.querySelector("p + figure")?.children.toString() ===
+        "[object HTMLTableElement]"
+      ) {
+        days = [daysHTML[0].innerText.toString()];
+      } else {
+        days = [daysHTML[1].innerText.toString()];
+      }
+    } else {
+      console.log("1 tabela");
+      days = [
+        daysHTML[0].innerText.toString(),
+        daysHTML[1].innerText.toString(),
+      ];
+    }
 
     const titles = days.map((_, index) => {
       return {
@@ -40,83 +57,53 @@ const main = async () => {
       };
     });
 
+    const parseMenu = (menu: any): string => {
+      return menu
+        .toString()
+        .split("\n")
+        .map((meal: string, index: number) => {
+          if (index === 0) {
+            return `\n*-* ${meal.trim()}`;
+          }
+          return `*-* ${meal.trim()}`;
+        })
+        .join("\n");
+    };
+
     const menus = days.map((_, index) => {
       return {
-        breakfast: breakfastHTML[index].innerText,
-        lunch: lunchHTML[index].innerText,
-        dinner: dinnerHTML[index].innerText,
+        breakfast: parseMenu(breakfastHTML[index].innerText),
+        lunch: parseMenu(lunchHTML[index].innerText),
+        dinner: parseMenu(dinnerHTML[index].innerText),
       };
     });
 
-    //const splitted = menus[0].lunch
-    //.toString()
-    //.split("\n")
-    //.map((meal: string) => meal.trim());
-    //console.log(splitted);
-
     let menuResponse = [];
-    //if (
-    //document.querySelector("p + figure")?.children.toString() ===
-    //"[object HTMLTableElement]"
-    //) {
 
-    //}
-
-    if (breakfastHTML.length === 2) {
-      menuResponse = days.map((_, index) => {
-        return {
-          weekDay: days[index],
-          breakfast: {
-            title: titles[index].breakfast,
-            menu: menus[index].breakfast,
-          },
-          lunch: {
-            title: titles[index].lunch,
-            menu: menus[index].lunch,
-          },
-          dinner: {
-            title: titles[index].dinner,
-            menu: menus[index].dinner,
-          },
-        };
-      });
-    } else {
-      let dayIndex: number;
-
-      // Checks if the first menu day has a table withe the menu
-      if (
-        document.querySelector("p + figure")?.children.toString() ===
-        "[object HTMLTableElement]"
-      ) {
-        dayIndex = 0;
-      } else {
-        dayIndex = 1;
-      }
-      menuResponse = [
-        {
-          weekDay: days[dayIndex],
-          breakfast: {
-            title: titles[dayIndex].breakfast,
-            menu: menus[dayIndex].breakfast,
-          },
-          lunch: {
-            title: titles[dayIndex].lunch,
-            menu: menus[dayIndex].lunch,
-          },
-          dinner: {
-            title: titles[dayIndex].dinner,
-            menu: menus[dayIndex].dinner,
-          },
+    menuResponse = days.map((_, index) => {
+      return {
+        weekDay: days[index],
+        breakfast: {
+          title: titles[index].breakfast,
+          menu: menus[index].breakfast,
         },
-      ];
-    }
+        lunch: {
+          title: titles[index].lunch,
+          menu: menus[index].lunch,
+        },
+        dinner: {
+          title: titles[index].dinner,
+          menu: menus[index].dinner,
+        },
+      };
+    });
 
     return menuResponse;
   });
 
   console.log(response);
 
-  //await browser.close();
+  await browser.close();
 };
 
 main();
